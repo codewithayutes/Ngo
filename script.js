c98e2c11f987892d507cae7230cacdb88bc958bf
codewithayutes <codewithayutes@gmail.com>
2025-04-03 23:55:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Enable Bootstrap Scrollspy feature
+document.addEventListener("DOMContentLoaded", function () {
+    let scrollSpy = new bootstrap.ScrollSpy(document.body, {
+        target: "#navbarNav",
+        offset: 100
+    });
+});
+
